7489fd7053c885a2b8e9e3e7349824b4b7c9d0ec
Carlos Marín <cemg1990@gmail.com>
2023-03-04 16:39:55 -0300

    Se perfecciona vista de de parrafos y se fixea el boton guardar

--- static/udla/js/alumno/proyecto/index.js
+++ static/udla/js/alumno/proyecto/index.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    $('#summernote').summernote();
     summernote_list.forEach(function (item) {
         $('#'+item).summernote({
             height: 600,
@@ -9,5 +8,4 @@ $(document).ready(function () {
               ]
         });
     });
-    
 });
